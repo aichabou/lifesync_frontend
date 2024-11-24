27467e6630b2313e69e1bf5b5b58798c259d824b
Aïcha BOUCHAIKH <aicha.bouchaikh@gmail.com>
2024-11-24 17:29:00 +0100

    "Added TaskManager component to App.js and created a new route for it"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import TaskManager from './pages/TaskManager';
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Routes>
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/tasks" element={<TaskManager />} />
             </Routes>
         </Router>
     );
